c906b5320226bc7e174489eb2213904bfe40a803
amk181 <amk181@txstate.edu>
2020-02-02 16:26:28 -0600

    initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+ * This is a simple Hello World in C++.
+ * Note the header structure and files.
+ * The only thing included here is main.h
+ * main.h contains all other directives
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Alex Katrompas
+ */
+
+#include "main.h"
+
+int main(int argc, char** argv) {
+    std::cout << "Hello, World!" << std::endl;
+
+    return 0;
+}
---
+++ main.h
@@ -0,0 +1,18 @@
+/*
+ * This is a simple Hello World in C++.
+ * Note the header structure and files.
+ * The only thing included in main.cpp is this main.h file.
+ * main.h contains all directives for main.cpp
+ */
+
+/* 
+ * File:   main.h
+ * Author: Alex Katrompas
+ */
+
+#ifndef HELLOWORLD_MAIN_H
+#define HELLOWORLD_MAIN_H
+
+#include <iostream>
+
+#endif //HELLOWORLD_MAIN_H
