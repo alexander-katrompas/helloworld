19adac7b4e2f378ef2a962fb21ef124aeebd0afa
Dorukhan Bekdur <dorukhanbekdur@gmail.com>
2024-02-21 23:35:06 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,19 +1,8 @@
-/*
- * This is a simple Hello World in C++.
- * Note the header structure and files.
- * The only thing included here is main.h
- * main.h contains all other directives
- */
 
-/* 
- * File:   main.cpp
- * Author: Alex Katrompas
- */
-
-#include "main.h"
-
-int main(int argc, char** argv) {
-    std::cout << "Hello, World!" << std::endl;
+#include <iostream>
+using namespace std;
+int main() {
+    cout << "Hello, World!";
 
     return 0;
 }
