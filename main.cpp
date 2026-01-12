aa0d593bcde3d31b7a1eb553377f08455c37a52c
Alexander Katrompas <alexander.katrompas@austincc.edu>
2026-01-12 16:32:03 +0200

    Revise comments and documentation in main.cpp
    Updated comments and documentation in main.cpp for clarity.

--- main.cpp
+++ main.cpp
@@ -1,18 +1,23 @@
-/*
- * This is a simple Hello World in C++.
- * Note the header structure and files.
- * The only thing included here is main.h
- * main.h contains all other directives
- */
-
-/* 
- * File:   main.cpp
- * Author: Alex Katrompas
- */
+/* *************************************************
+*  Name: Alexander Katrompas
+*  Assignment: Demonstration Code
+*  Purpose: A demonstration of a properly
+*           constructed and commented main.cpp
+************************************************* */
 
 #include "main.h"
 
 int main(int argc, char** argv) {
+    /* **********************************
+     * This function is the application driver. It uses
+     * and tests the functions.py and Square.py modules.
+     *
+     * @param na : na
+     * @return (int) : application exit code
+     * @exception na : na
+     * @note na
+     * **********************************/
+   
     std::cout << "Hello, World!" << std::endl;
 
     return 0;
